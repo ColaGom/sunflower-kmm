104ab360ae66e73440edbc06a535aa2964e75208
ColaGom <bchoi000@gmail.com>
2023-04-25 14:11:40 +0900

    Update swift file

--- Package.swift
+++ Package.swift
@@ -4,22 +4,19 @@ import PackageDescription
 let package = Package(
     name: "Shared",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Shared",
             targets: ["Shared"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "Shared",
             url: "https://maven.pkg.github.com/ColaGom/sunflower-kmm/com/samples/apps/sunflower/shared/1.0.0/shared-1.0.0.zip",
-            checksum: "3e4279c413072f5f65017ef7013708f42883793b7c06f36a1027c2f9da71663f"
+            checksum: "f4df5f9f3a6bf35754fc157fad012e5cdbff8bf47b2d92b8e35840406344ffb6"
         )
     ]
 )
